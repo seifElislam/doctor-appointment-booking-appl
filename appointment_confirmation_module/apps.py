160cbc49baa59fcfc7ac28668386779cd00dd2c9
Muhammed-Ahmed-Ismail <muhammadahmed.free@gmail.com>
2025-01-11 16:05:45 +0200

    feat: start appointment comfirmation module

---
+++ appointment_confirmation_module/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class AppointmentConfirmationModuleConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'appointment_confirmation_module'
