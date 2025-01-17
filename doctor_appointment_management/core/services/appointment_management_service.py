e05b5bdc2be702dc2256cc268ee4b8733dcb1d50
KareemOsama15 <karim.osama.saleh@gmail.com>
2025-01-17 15:25:36 +0200

    feat: add service for business logic implements doctor management on appointments

---
+++ doctor_appointment_management/core/services/appointment_management_service.py
@@ -0,0 +1,19 @@
+from doctor_appointment_management_module.core.inputports.appointment_management_service_port import \
+AppointmentManagementServicePort
+from doctor_appointment_management_module.core.outputports.appointment_management_repo_interface import \
+AppointmentManagementRepoInterface
+
+
+class AppointmentManagementService(AppointmentManagementServicePort):
+
+    def __init__(self, repository: AppointmentManagementRepoInterface):
+        self.repository = repository
+
+    def get_schedeuled_appointments(self) -> list:
+        return self.repository.get_schedeuled()
+
+    def cancel_appointment(self, appointment_id: int) -> None:
+        self.repository.cancel(appointment_id)
+
+    def mark_appointment_as_completed(self, appointment_id: int) -> None:
+        self.repository.complete(appointment_id)
