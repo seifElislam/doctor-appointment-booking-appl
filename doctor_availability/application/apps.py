e0cd71130cb12251b2e9302f0fadc81d4bf805ff
SeifHegazy <seif.h@trufla.com>
2025-01-25 12:46:54 +0200

    merge domain and application layer due to simple business logic

--- doctor_availability/application/apps.py
+++ doctor_availability/application/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class DoctorAvailabilityConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'doctor_availability'
+
+    def ready(self):
+        import doctor_availability.infrastructure.models
---
+++ doctor_availability/application/services.py
@@ -0,0 +1,27 @@
+"""
+Module use cases
+"""
+
+
+def list_doctors():
+    pass
+
+
+def add_doctor():
+    pass
+
+
+def get_doctor_by_id():
+    pass
+
+
+def list_doctor_avail_slots():
+    pass
+
+
+def add_slot():
+    pass
+
+
+def update_slot():
+    pass
