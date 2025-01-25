f2e41a1000dc24bdb7478110a5aa5395b3ee2744
KareemOsama15 <karim.osama.saleh@gmail.com>
2025-01-25 18:20:49 +0200

    ref: doctor appointment management module urls

--- doctor_appointment_management/shell/apis/urls.py
+++ doctor_appointment_management/shell/apis/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 from .views import AppointmentScheduledView, AppointmentCompleteView, AppointmentCancelView
 
 urlpatterns = [
-    path('appointments/', AppointmentScheduledView.as_view(), 
+    path('schedueled/', AppointmentScheduledView.as_view(), 
          name='scheduled-appointments'),
-    path('appointment/<int:id>/cancel/', AppointmentCancelView.as_view(),
+    path('<int:id>/cancel/', AppointmentCancelView.as_view(),
          name='cancel-appointment'),
-    path('appointment/<int:id>/complete/', AppointmentCompleteView.as_view(),
+    path('<int:id>/complete/', AppointmentCompleteView.as_view(),
          name='complete-appointment'),
 ]
