d12c48466fda903aff5f621464b3e3312dd62bd2
SeifHegazy <seif.h@trufla.com>
2025-01-25 10:52:58 +0200

    add module db models

--- doctor_availability/infrastructure/models.py
+++ doctor_availability/infrastructure/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class Doctor(models.Model):
+    """
+    Doctor database model
+    """
+    name = models.CharField(max_length=100, blank=False)
+    specialization = models.CharField(max_length=100, blank=True)
+
+
+class Slot(models.Model):
+    """
+    Doctor availability slot data model
+    """
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    doctor = models.ForeignKey(Doctor, on_delete=models.CASCADE)
+    is_reserved = models.BooleanField(default=False)
+    cost = models.DecimalField()
